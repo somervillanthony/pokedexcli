5c0fb0c9c8337fa115f531e9c14cc0ace5ddd64e
Rezalex <somervillanthony@gmail.com>
2026-05-13 16:59:53 -0400

    refactored code so it wasn't a unconventional mess

---
+++ repl.go
@@ -0,0 +1,59 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strings"
+)
+
+func startRepl() {
+	scanner := bufio.NewScanner(os.Stdin)
+	for {
+		fmt.Print("Pokedex >")
+		scanner.Scan()
+		inputString := scanner.Text()
+
+		inputString = strings.ToLower(inputString)
+		inputSlice := strings.Fields(inputString)
+
+		commands := getCommands()
+
+		command, ok := commands[inputSlice[0]]
+		if ok {
+			err := command.callback()
+			if err != nil {
+				fmt.Println(err)
+			}
+		} else {
+			fmt.Println("Unknown command")
+		}
+
+	}
+}
+
+func cleanInput(text string) []string {
+	lwrText := strings.ToLower(text)
+	return strings.Fields(lwrText)
+}
+
+type cliCommand struct {
+	name        string
+	description string
+	callback    func() error
+}
+
+func getCommands() map[string]cliCommand {
+	return map[string]cliCommand{
+		"exit": {
+			name:        "exit",
+			description: "Exit the Pokedex",
+			callback:    commandExit,
+		},
+		"help": {
+			name:        "help",
+			description: "Displays a help message",
+			callback:    commandHelp,
+		},
+	}
+}
